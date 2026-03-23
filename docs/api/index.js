c5c45f19aba64c914f98ac7689cc0bef9dda4fc7
jbh010204 <jbh010204@naver.com>
2026-03-24 02:32:31 +0900

    docs: API 문서 업데이트

--- docs/api/index.js
+++ docs/api/index.js
@@ -1,9 +1,30 @@
 window.API_DOCS = [
 {
+  title: "CommentControllerApi",
+  summary: "댓글 작성",
+  href: "./comment/CommentControllerApi.html",
+  endpointCount: 5,
+  sectionPath: "comment"
+},
+{
+  title: "MemberControllerApi",
+  summary: "내 프로필 조회",
+  href: "./member/MemberControllerApi.html",
+  endpointCount: 3,
+  sectionPath: "member"
+},
+{
   title: "PaymentControllerApi",
   summary: "결제 승인",
   href: "./payment/PaymentControllerApi.html",
   endpointCount: 2,
   sectionPath: "payment"
+},
+{
+  title: "PostControllerApi",
+  summary: "게시글 작성",
+  href: "./post/PostControllerApi.html",
+  endpointCount: 6,
+  sectionPath: "post"
 }
 ];
